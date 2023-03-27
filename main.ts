dfcc653329b426b28e4665f47edf86a674b00b58
Jeremy <chuya@honsha.localdomain>
2023-03-27 15:25:28 +0000

    ✨ feat: :art: Logger colors

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import logger from './logger';
+import { logger } from './logger';
 
 // Importez vos modules ici
 // import CommandRecognition from './CommandRecognition';
@@ -7,21 +7,26 @@ import logger from './logger';
 // import GPT3Request from './GPT3Request';
 
 async function main() {
-  logger.info('Démarrage de l\'application "Yui"');
+    logger.info('Démarrage de l\'application "Yui"');
+    logger.info('Message d\'information');
+    logger.success('Message de succès');
+    logger.warn('Message d\'avertissement');
+    logger.error('Message d\'erreur');
+    logger.debug('Message de débogage');
 
-  // Initialisez vos modules ici
-  // const commandRecognition = new CommandRecognition();
-  // const manualCommand = new ManualCommand();
-  // const commandExecutor = new CommandExecutor();
-  // const gpt3Request = new GPT3Request();
+    // Initialisez vos modules ici
+    // const commandRecognition = new CommandRecognition();
+    // const manualCommand = new ManualCommand();
+    // const commandExecutor = new CommandExecutor();
+    // const gpt3Request = new GPT3Request();
 
-  logger.info('Initialisation des modules terminée');
+    logger.info('Initialisation des modules terminée');
 }
 
 main()
-  .then(() => {
-    logger.info('L\'application "Yui" est en cours d\'exécution');
-  })
-  .catch((error) => {
-    logger.error(`Erreur lors de l'exécution de l'application : ${error.message}`);
-  });
+    .then(() => {
+        logger.info('L\'application "Yui" est en cours d\'exécution');
+    })
+    .catch((error) => {
+        logger.error(`Erreur lors de l'exécution de l'application : ${error.message}`);
+});
