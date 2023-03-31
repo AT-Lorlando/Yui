1c1a2ebf8258d885ea86b0a719fdafbb11404038
AT-Lorlando <richard-jeremy@outlook.fr>
2023-03-31 10:23:29 +0200

    :art: Run prettier & linter

--- Entity.ts
+++ Entity.ts
@@ -1,7 +1,8 @@
+import { logger } from './logger';
+import * as fs from 'fs';
+
 export abstract class Entity {
-    constructor(
-        public name: string
-    ) {}
+    constructor(public name: string) {}
 
     abstract shutdown(): void;
     abstract startup(): void;
@@ -10,12 +11,10 @@ export abstract class Entity {
         console.log(`Test of ${this.name}`);
     }
 }
-  
-export class Light extends Entity {
 
+export class Light extends Entity {
     constructor(name: string, public room: string) {
         super(name);
-        
     }
 
     shutdown(): void {
@@ -31,7 +30,7 @@ export class Light extends Entity {
 
 export class TV extends Entity {
     constructor(name: string, public room: string) {
-        super(name);   
+        super(name);
     }
 
     shutdown(): void {
@@ -60,6 +59,39 @@ export class Speakers extends Entity {
         // Ajoutez ici le code pour allumer les haut-parleurs
     }
 }
-  
-  // Ajoutez d'autres classes pour d'autres entités ici
-  
+
+// Ajoutez d'autres classes pour d'autres entités ici
+
+export async function testEntities(entities: Entity[]) {
+    entities.forEach((entity) => {
+        entity.test();
+    });
+}
+
+export async function initEntities() {
+    // Read from entities.json and create the entities
+
+    // Return an array of entities
+
+    const entitiesArray: Entity[] = [];
+    const entitiesJson = fs.readFileSync('entities.json', 'utf8');
+    const entities = JSON.parse(entitiesJson);
+    entities.lights.forEach((light: any) => {
+        entitiesArray.push(new Light(light.name, light.room));
+        logger.info(`Light '${light.name}' in ${light.room} added`);
+    });
+    entities.speakers.forEach((speakers: any) => {
+        entitiesArray.push(new Speakers(speakers.name, speakers.room));
+        logger.info(`Speakers '${speakers.name}' in ${speakers.room} added`);
+    });
+    entities.devices.forEach((device: any) => {
+        if (device.type === 'TV') {
+            entitiesArray.push(new TV(device.name, device.room));
+            logger.info(`TV '${device.name}' in ${device.room} added`);
+        } else {
+            logger.error(`Unknown device type: ${device.name}`);
+        }
+    });
+
+    return entitiesArray;
+}
