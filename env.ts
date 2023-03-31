b3f7277f8a92f81b58d6068d3652c352539448b8
AT-Lorlando <richard-jeremy@outlook.fr>
2023-03-31 10:23:29 +0200

    🎨 Run prettier & linter

--- env.ts
+++ env.ts
@@ -1,15 +1,16 @@
-require('dotenv').config({ path: '.env' });
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 export interface Env {
-  NODE_ENV: 'development' | 'production';
-  OPENAI_API_KEY: string;
-  LOG_LEVEL: 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly';
+    NODE_ENV: 'development' | 'production';
+    OPENAI_API_KEY: string;
+    LOG_LEVEL: 'error' | 'warn' | 'info' | 'verbose' | 'debug' | 'silly';
 }
 
 export const env: Env = {
-  NODE_ENV: process.env.NODE_ENV as Env['NODE_ENV'],
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY as Env['OPENAI_API_KEY'],
-  LOG_LEVEL: process.env.LOG_LEVEL as Env['LOG_LEVEL'],
+    NODE_ENV: process.env.NODE_ENV as Env['NODE_ENV'],
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY as Env['OPENAI_API_KEY'],
+    LOG_LEVEL: process.env.LOG_LEVEL as Env['LOG_LEVEL'],
 };
 
 export default env;
