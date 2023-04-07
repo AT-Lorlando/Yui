e064f6ff718030ccd8e9bddb277645c9ae2ded64
AT-Lorlando <richard-jeremy@outlook.fr>
2023-04-07 10:23:51 +0200

    :art: Run lint

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,25 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/test/*.*spec.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  projects: [
-    {
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      displayName: 'unit-integration',
-      testMatch: ['<rootDir>/test/*.unit-spec.ts', '<rootDir>/test/*.int-spec.ts'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/test/*.*spec.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
     },
-    {
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      displayName: 'e2e',
-      testMatch: ['<rootDir>/test/*.e2e-spec.ts'],
-    },
-  ],
+    projects: [
+        {
+            preset: 'ts-jest',
+            testEnvironment: 'node',
+            displayName: 'unit-integration',
+            testMatch: [
+                '<rootDir>/test/*.unit-spec.ts',
+                '<rootDir>/test/*.int-spec.ts',
+            ],
+        },
+        {
+            preset: 'ts-jest',
+            testEnvironment: 'node',
+            displayName: 'e2e',
+            testMatch: ['<rootDir>/test/*.e2e-spec.ts'],
+        },
+    ],
 };
