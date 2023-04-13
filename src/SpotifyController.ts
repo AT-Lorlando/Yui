05f0a0c69fcbcef49b96d31077c2aba880e53c9f
AT-Lorlando <richard-jeremy@outlook.fr>
2023-04-13 11:17:34 +0200

    :sparkles: Spotify Auth w/ token & token management

--- src/SpotifyController.ts
+++ src/SpotifyController.ts
@@ -3,9 +3,12 @@ import env from './env';
 import { Client, DefaultMediaReceiver } from 'castv2-client';
 import { logger } from './logger';
 import Bonjour from 'bonjour';
+import * as fs from 'fs';
+import axios from 'axios';
 
 export default class SpotifyController {
     private spotifyApi: SpotifyWebApi;
+    private flag = false;
 
     constructor() {
         this.spotifyApi = new SpotifyWebApi({
@@ -15,10 +18,142 @@ export default class SpotifyController {
         });
     }
 
-    public async init(): Promise<void> {}
+    public async init(): Promise<void> {
+        const refreshToken = await this.loadRefreshToken();
+        if (refreshToken !== null) {
+            try {
+                const access_token = await this.refreshAccessToken(
+                    refreshToken,
+                );
+                this.spotifyApi.setAccessToken(access_token);
+                logger.info('Yui is now authorized');
+            } catch (error) {
+                logger.error('Error refreshing access token:', error);
+            }
+        } else {
+            const clientId = env.SPOTIFY_CLIENT_ID;
+            const redirectUri = env.SPOTIFY_REDIRECT_URI;
+            const scopes = [
+                'app-remote-control',
+                'user-modify-playback-state',
+                'user-read-playback-state',
+            ];
+            const authorizeUrl = this.generateAuthorizeUrl(
+                clientId,
+                redirectUri,
+                scopes,
+            );
+            logger.info(`Please go to ${authorizeUrl} and authorize Yui`);
+            await this.isAuthorized();
+            logger.info('Yui is now authorized');
+        }
+    }
+
+    private async isAuthorized(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            let intervalId: NodeJS.Timeout;
+
+            const checkFlag = () => {
+                if (this.flag) {
+                    clearInterval(intervalId);
+                    resolve();
+                }
+            };
+
+            intervalId = setInterval(checkFlag, 5000);
+
+            setTimeout(() => {
+                clearInterval(intervalId);
+                if (!this.flag) {
+                    reject(
+                        new Error('Flag did not turn true within 30 seconds'),
+                    );
+                }
+            }, 30 * 1000);
+        });
+    }
 
     async setAccessToken(accessToken: string): Promise<void> {
         this.spotifyApi.setAccessToken(accessToken);
+        this.flag = true;
+    }
+
+    async saveRefreshToken(refreshToken: string): Promise<void> {
+        const data = { refreshToken };
+        await fs.promises.writeFile('refresh-token.json', JSON.stringify(data));
+    }
+
+    async loadRefreshToken(): Promise<string | null> {
+        try {
+            const data = await fs.promises.readFile(
+                'refresh-token.json',
+                'utf-8',
+            );
+            return JSON.parse(data).refreshToken;
+        } catch (error) {
+            console.error('Error loading refresh token:', error);
+            return null;
+        }
+    }
+
+    async exchangeAuthorizationCode(code: string): Promise<{
+        accessToken: string;
+        refreshToken: string;
+    }> {
+        const clientId = env.SPOTIFY_CLIENT_ID;
+        const clientSecret = env.SPOTIFY_CLIENT_SECRET;
+        const redirectUri = env.SPOTIFY_REDIRECT_URI;
+        const tokenEndpoint = 'https://accounts.spotify.com/api/token';
+        const authString = Buffer.from(`${clientId}:${clientSecret}`).toString(
+            'base64',
+        );
+        const headers = {
+            Authorization: `Basic ${authString}`,
+        };
+        const body = new URLSearchParams({
+            grant_type: 'authorization_code',
+            code: code,
+            redirect_uri: redirectUri,
+        });
+
+        try {
+            const response = await axios.post(tokenEndpoint, body.toString(), {
+                headers: headers,
+            });
+            return {
+                accessToken: response.data.access_token,
+                refreshToken: response.data.refresh_token,
+            };
+        } catch (error) {
+            console.error('Error exchanging authorization code:', error);
+            throw new Error('Error exchanging authorization code');
+        }
+    }
+
+    async refreshAccessToken(refreshToken: string): Promise<string> {
+        const clientId = env.SPOTIFY_CLIENT_ID;
+        const clientSecret = env.SPOTIFY_CLIENT_SECRET;
+        const tokenEndpoint = 'https://accounts.spotify.com/api/token';
+        const authString = Buffer.from(`${clientId}:${clientSecret}`).toString(
+            'base64',
+        );
+        const headers = {
+            Authorization: `Basic ${authString}`,
+        };
+        const body = new URLSearchParams({
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken,
+        });
+
+        try {
+            const response = await axios.post(tokenEndpoint, body.toString(), {
+                headers: headers,
+            });
+            return response.data.access_token;
+        } catch (error) {
+            console.error('Error refreshing access token:', error);
+            throw new Error('Error refreshing access token');
+        }
     }
 
     async play(): Promise<void> {
