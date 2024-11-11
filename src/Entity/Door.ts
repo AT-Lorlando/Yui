b80b2e07dac133d4068f2e3cbc872003e87bb526
AT-Lorlando <rcd.jeremy@gmail.com>
2024-11-11 12:55:32 +0000

    :recycle: Refacto entities

---
+++ src/Entity/Door.ts
@@ -0,0 +1,17 @@
+import { Entity } from '../Entity';
+
+export class Door extends Entity {
+    constructor(name: string, public id: number, public room: string) {
+        super(name, id, room);
+    }
+
+    async lock(): Promise<void> {
+        console.log(`The door is open.`);
+        // Ajoutez ici le code pour ouvrir la porte
+    }
+
+    async unlock(): Promise<void> {
+        console.log(`The door is closed.`);
+        // Ajoutez ici le code pour fermer la porte
+    }
+}
