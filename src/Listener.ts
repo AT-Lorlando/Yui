6112a53416ee612be6a0d448a115df4441135c8a
AT-Lorlando <rcd.jeremy@gmail.com>
2024-11-17 10:00:52 +0000

    ✨ Payload listener

--- src/Listener.ts
+++ src/Listener.ts
@@ -4,12 +4,15 @@ import * as fs from 'fs';
 import CommandExecutor from './CommandExecutor';
 import { logger } from './logger';
 import env from './env';
-import axios from 'axios';
-import https from 'https';
+import cors from 'cors';
 
-const BBOX_PASSWORD = env.BBOX_PASSWORD;
-const PHONE_MAC_ADDRESS = env.PHONE_MAC_ADDRESS;
-const PHONE_CHECK_INTERVAL = 10;
+// const BBOX_PASSWORD = env.BBOX_PASSWORD;
+// const PHONE_MAC_ADDRESS = env.PHONE_MAC_ADDRESS;
+// const PHONE_CHECK_INTERVAL = 10;
+// const HOME_POSITION = {
+//     latitude: parseFloat(env.HOME_LATITUDE),
+//     longitude: parseFloat(env.HOME_LONGITUDE),
+// };
 
 export default class Listener {
     private commandExecutor!: CommandExecutor;
@@ -19,7 +22,6 @@ export default class Listener {
             this.commandExecutor = commandExecutor;
             await this.listenOnWeb();
             await this.listenOnStdin();
-            await this.monitorUserPresence();
         } catch (error) {
             logger.error(
                 `Error during the initialisation of Listener: ${error}`,
@@ -28,10 +30,37 @@ export default class Listener {
         }
     }
 
+    // private isBannedIP(ip: string): boolean {
+    //     try {
+    //         const bannedIPs = fs.readFileSync('banned_ips.txt', 'utf8');
+    //         return bannedIPs.includes(ip);
+    //     } catch (error) {
+    //         throw error;
+    //     }
+    // }
+
+    private checkPassword(bearer: string | undefined, ip: string): boolean {
+        // Todo: Add a rate limiter
+        if (bearer === undefined || bearer !== env.BEARER_TOKEN) {
+            logger.error('Wrong password');
+            logger.error(`Banned IP: ${ip}`);
+            fs.appendFileSync('banned_ips.txt', `${ip}\n`);
+            return false;
+        }
+        return true;
+    }
+
     private async listenOnWeb(): Promise<void> {
         return new Promise((resolve, reject) => {
             const port = 3000;
             const app = express();
+            app.use(
+                cors({
+                    origin: '*', // Autoriser toutes les origines
+                    // methods: ['GET', 'POST'], // Autoriser uniquement les méthodes GET et POST
+                    // allowedHeaders: ['Content-Type', 'Authorization'] // Autoriser uniquement certains en-têtes
+                }),
+            );
             app.use(bodyParser.json());
             app.use(bodyParser.urlencoded({ extended: true }));
             if (!fs.existsSync('banned_ips.txt')) {
@@ -40,40 +69,31 @@ export default class Listener {
 
             app.post('/command', (req: any, res: any) => {
                 if (this.commandExecutor === undefined) {
-                    throw new Error('CommandExecutor is undefined');
+                    res.status(500).send('CommandExecutor is down');
+                    throw new Error(
+                        'CommandExecutor is undefined, cannot execute command',
+                    );
                 }
                 const ip =
                     req.headers['x-forwarded-for'] ||
                     req.connection.remoteAddress;
-                try {
-                    if (
-                        fs.readFileSync('banned_ips.txt', 'utf8').includes(ip)
-                    ) {
-                        logger.error(`Post from banned IP: ${ip}`);
-                        res.status(401).send('Banned IP');
-                        return;
-                    }
-                } catch (error) {
-                    throw error;
-                }
                 const bearer = req.headers.authorization;
-
-                if (bearer === undefined || bearer !== env.BEARER_TOKEN) {
-                    logger.error('Wrong password');
-                    logger.error(`Banned IP: ${ip}`);
-                    logger.error(req.body);
-                    fs.appendFileSync('banned_ips.txt', `${ip}\n`);
-                    res.status(401).send('Wrong password');
-                    this.commandExecutor.PushNotification(
-                        'Yui - Alert',
-                        'Unauthorized request, IP banned' + ip,
-                    );
-                    return;
-                }
                 const command = req.body.command;
                 logger.info(
                     `WEB Listener: Received order ${command.replace('\n', '')}`,
                 );
+
+                // if (this.isBannedIP(ip)) {
+                //     logger.error(`Banned IP: ${ip}`);
+                //     res.status(401).send('Unauthorized');
+                //     return;
+                // }
+                if (!this.checkPassword(bearer, ip)) {
+                    logger.error(`Wrong password from IP: ${ip}`);
+                    res.status(401).send('Unauthorized');
+                    return;
+                }
+
                 try {
                     switch (command) {
                         case 'backhome':
@@ -99,13 +119,60 @@ export default class Listener {
             });
 
             app.get('/', (req: any, res: any) => {
-                // send assets/index.html
                 res.status(200).sendFile('assets/index.html', {
                     root: __dirname,
                 });
                 console.log(req.query.code);
                 if (req.query.code) {
                     this.commandExecutor.spotifyAuth(req.query.code);
+                }
+            });
+
+            app.post('/payload', (req: any, res: any) => {
+                if (this.commandExecutor === undefined) {
+                    res.status(500).send('CommandExecutor is down');
+                    throw new Error(
+                        'CommandExecutor is undefined, cannot execute command',
+                    );
+                }
+                // const ip =
+                //     req.headers['x-forwarded-for'] ||
+                //     req.connection.remoteAddress;
+                // const bearer = req.headers.authorization;
+                const payload = req.body;
+                logger.info(
+                    `WEB Listener: Received payload ${JSON.stringify(payload)}`,
+                );
+
+                // if (this.isBannedIP(ip)) {
+                //     logger.error(`Banned IP: ${ip}`);
+                //     res.status(401).send('Unauthorized');
+                //     return;
+                // }
+                // if (!this.checkPassword(bearer, ip)) {
+                //     logger.error(`Wrong password from IP: ${ip}`);
+                //     res.status(401).send('Unauthorized');
+                //     return;
+                // }
+
+                try {
+                    // Analyse the payload
+                    // The payload should be a JSON object with the following structure:
+                    // {
+                    //     "id": "unique_entity_id",
+                    //     "position": {
+                    //         "latitude": 48.123456,
+                    //         "longitude": 2.123456
+                    //     },
+                    //     "status": "status"
+                    // }
+                    logger.info(`Payload received: ${JSON.stringify(payload)}`);
+                    res.status(200).send('Payload received');
+                    return;
+                } catch (error: any) {
+                    logger.error(error.message);
+                    res.status(500).send(error.message);
+                    return;
                 }
             });
 
@@ -136,88 +203,5 @@ export default class Listener {
                 );
             }
         });
-    }
-
-    async isPhoneConnected() {
-        const agent = new https.Agent({
-            rejectUnauthorized: false,
-        });
-        const loginResponse = await axios
-            .post(
-                'https://mabbox.bytel.fr/api/v1/login',
-                `password=${BBOX_PASSWORD}`,
-                { httpsAgent: agent },
-            )
-            .catch((error) => {
-                logger.error(`Failed to login to Bbox API: ${error}`);
-                return error.response;
-            });
-
-        if (loginResponse.status !== 200) {
-            throw new Error('Failed to login to Bbox API');
-        }
-
-        const devicesResponse = await axios.get(
-            'https://mabbox.bytel.fr/api/v1/hosts',
-            {
-                headers: {
-                    Cookie: loginResponse.headers['set-cookie'],
-                },
-            },
-        );
-
-        if (devicesResponse.status !== 200) {
-            throw new Error('Failed to fetch connected devices from Bbox API');
-        }
-
-        // Check if the phone is connected
-        // console.log(devicesResponse.data[0].hosts.list)
-        const phone = devicesResponse.data[0].hosts.list.find(
-            (device: any) => device.macaddress === PHONE_MAC_ADDRESS,
-        );
-        if (phone) {
-            // we use the "active" field to determine if the phone is connected
-            const isConnected = phone.active === 1;
-            logger.silly(
-                `Phone is ${isConnected ? 'connected' : 'disconnected'}`,
-            );
-            const now = Math.floor(new Date().getTime() / 1000); // current time in seconds since the epoch
-            const lastSeenSeconds = parseInt(phone.lastseen, 10); // second since the epoch
-            const lastSeen = new Date((now - lastSeenSeconds) * 1000); // convert to milliseconds
-            logger.silly(`Last seen: ${lastSeen.toLocaleString()}`);
-            return {
-                isConnected: isConnected,
-                lastSeen: lastSeen,
-            };
-        } else {
-            throw new Error(
-                `Phone with MAC address ${PHONE_MAC_ADDRESS} not found in device list`,
-            );
-        }
-    }
-
-    async monitorUserPresence() {
-        let { isConnected: wasConnected } = await this.isPhoneConnected();
-        setInterval(async () => {
-            try {
-                const { isConnected, lastSeen } = await this.isPhoneConnected();
-                if (isConnected !== wasConnected) {
-                    if (isConnected) {
-                        logger.info(
-                            `User returned at ${lastSeen.toLocaleString()}`,
-                        );
-                        this.commandExecutor.backHome();
-                    } else {
-                        logger.info(
-                            `User left at ${lastSeen.toLocaleString()}`,
-                        );
-                        this.commandExecutor.leaveHome();
-                    }
-                    wasConnected = isConnected;
-                }
-            } catch (error) {
-                logger.error(`Failed to check phone connection: ${error}`);
-            }
-        }, PHONE_CHECK_INTERVAL * 1000);
     }
 }
