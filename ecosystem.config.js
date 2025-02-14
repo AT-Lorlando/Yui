5c3ed9e84e9eedd82aa43b48ecf34e67961d87ae
Jérémy <rcd.jeremy@gmail.com>
2025-02-14 13:21:26 +0100

    :wrench: Some config updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
     apps: [
-      {
-        name: 'Yui',
-        script: 'npx ts-node src/main.ts',
-        env: {
-          NODE_ENV: 'production',
-          LOG_LEVEL: 'info',
+        {
+            name: 'Yui',
+            script: 'npx ts-node src/main.ts',
+            env: {
+                NODE_ENV: 'production',
+                LOG_LEVEL: 'info',
+            },
         },
-      },
     ],
-  };
-  
+};
