0ed5530f101926393c2a585f98c888959930698c
Jérémy <rcd.jeremy@gmail.com>
2025-02-19 16:49:33 +0100

    :recycle: update removeJsonComments function to strip code block markers from JSON strings

--- src/Service/JsonUtils.ts
+++ src/Service/JsonUtils.ts
@@ -2,6 +2,5 @@ export function removeJsonComments(jsonString: string): string {
     // Regex to match single-line comments (//) and multi-line comments (/* */)
     // const regex = /\/\/.*|\/\*[\s\S]*?\*\//g;
     // return jsonString.replace(regex, '');
-
-    return jsonString;
+    return jsonString.replace('```json', '').replace('```', '');
 }
